42b296b3b532f57b9034e49c982ce4869ef38173
acoushawk <mattarmstrong430@gmail.com>
2017-12-08 23:25:26 -0500

    Progress. Need to adjust percentage type to float.

--- api.go
+++ api.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gorilla/mux"
+	"html/template"
 	"net/http"
 )
 
@@ -10,11 +11,27 @@ type APIConfig struct {
 	port      int
 }
 
+type Person struct {
+	UserName string
+}
+
+type APIStatus struct {
+	APIFiles   []APIFile
+	TotalFiles int
+	Percentage int
+}
+
+type APIFile struct {
+	FileName string
+	Uploaded bool
+}
+
 func api() {
 
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
 	r.HandleFunc("/", files)
+	r.HandleFunc("/temp", testtemplate)
 
 	// Bind to a port and pass our router in
 	http.ListenAndServe(":8000", r)
@@ -43,6 +60,34 @@ func files(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func test(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Gorilla!\n"))
+func testtemplate(w http.ResponseWriter, r *http.Request) {
+	var fileStatus APIStatus
+	var completedFiles, totalFiles int
+	t := template.Must(template.ParseFiles("/Users/matthewarmstrong/scripts/backblaze-go/src/backblaze/static/main.html"))
+	// files := fileCompleteQueue.Files
+	for _, folder := range config.Folders {
+		listFiles := getFiles(folder.RootFolder)
+		totalFiles = len(listFiles)
+		completedFiles = 0
+		for _, file := range listFiles {
+			var fileStat APIFile
+			var found bool
+			for _, file2 := range folder.b2Files.Files {
+				if (folder.B2Folder + file) == file2.FileName {
+					found = true
+				}
+			}
+			if !found {
+				fileStat.Uploaded = false
+			} else {
+				completedFiles++
+				fileStat.Uploaded = true
+			}
+			fileStat.FileName = file[1:]
+			fileStatus.APIFiles = append(fileStatus.APIFiles, fileStat)
+		}
+		fileStatus.TotalFiles = totalFiles
+		fileStatus.Percentage = (completedFiles / totalFiles) //* 100
+		t.Execute(w, fileStatus)
+	}
 }
